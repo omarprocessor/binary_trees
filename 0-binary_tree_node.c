dbb6c775e21042b92de28aa91ba1e4df1b1a60c6
omarprocessor <wadaniinarais1@gmail.com>
2024-12-04 06:49:32 -0800

    task done

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
-/*
- * *binary_tree_node - Creates a new binary tree node.
+/**
+ * binary_tree_node - Creates a new binary tree node.
  * @parent: Pointer to the parent node of the new node.
  * @value: Value to be stored in the new node.
  *
